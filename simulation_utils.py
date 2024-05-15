860433820caf10f98854e0d90f5eb7457b99623c
Thomas Penny <thomaspenny284@gmail.com>
2024-05-15 11:32:10 -0400

    Fixed bugs in transfer function functions

--- simulation_utils.py
+++ simulation_utils.py
@@ -1,18 +1,21 @@
 import numpy as np
 
-def transfer_function(w, w0, y0, yfb):
+def transfer_function(w, w0, y0, yfb, M, T):
     """
     Exact transfer function of particle
     w: frequency bins for frequency domain response
     w0: natural frequency of particle
     y0: intrinsic damping of particle (gas and/or laser)
     yfb: Additional damping from cold feedback mechanism
+    M: mass of oscillator
+    T: environment temperature
     """
+    kb = 1.38*10**(-23) # Boltzmann constant
     A = np.sqrt(4*kb*T*y0*M)
     chi = (M*(w0**2-w**2-1j*w*(y0+yfb)))**(-1)
     return A*chi
 
-def transfer_function2(w, w0, y0, yfb, rnd, rnd2):
+def transfer_function2(w, w0, y0, yfb, rnd, rnd2, M, T):
     """
     Transfer function of particle with noise for finite time measurement
     w: frequency bins for frequency domain response
@@ -20,7 +23,10 @@ def transfer_function2(w, w0, y0, yfb, rnd, rnd2):
     y0: intrinsic damping of particle (gas and/or laser)
     yfb: Additional damping from cold feedback mechanism
     rnd and rnd2: Noise for fourier domain - should each have 0 mean and 1/sqrt(2) width I think
+    M: mass of oscillator
+    T: environment temperature
     """
+    kb = 1.38*10**(-23) # Boltzmann constant
     A = np.sqrt(4*kb*T*y0*M)
     chi = (M*(w0**2-w**2-1j*w*(y0+yfb)))**(-1)
     return A*chi*(rnd+1j*rnd2)
@@ -45,7 +51,7 @@ def impulse_resp(time, t0, A, y0, yfb, w0):
             output[n] =  A*np.sin(w1*(t-t0))*np.exp(-y1*(t-t0))
     return output
 
-def generate_displacement(w, w0, y0, yfb, rnd, rnd2, rnd3, ir):
+def generate_displacement(w, w0, y0, yfb, M, T, rnd, rnd2, rnd3, ir):
     """
     Generates the frequency domain response of a particle then reverse fourier transforms to the time domain
     Adds an impulse response and imprecision noise in the time domain
@@ -54,6 +60,8 @@ def generate_displacement(w, w0, y0, yfb, rnd, rnd2, rnd3, ir):
     w0: natural frequency of particle
     y0: intrinsic damping of particle (gas and/or laser)
     yfb: Additional damping from cold feedback mechanism
+    M: mass of oscillator
+    T: environment temperature
     rnd and rnd2: Noise for fourier domain - should each have 0 mean and 1/sqrt(2) width I think
     rnd3: imprecision noise - should be 0 mean with width of Snn/2/dtn where Snn of single-sided PSD noise value in m^2/Hz and dtn is timestep in s (1/(2*(max frequency in Hz)))
     ir: impulse response - must have same number of points as 2*len(w)
@@ -61,7 +69,7 @@ def generate_displacement(w, w0, y0, yfb, rnd, rnd2, rnd3, ir):
 
     numbins = len(w)
     # Generate frequency response of particle
-    thermal_response = transfer_function2(w, w0, y0, yfb, rnd, rnd2)
+    thermal_response = transfer_function2(w, w0, y0, yfb, rnd, rnd2, M, T)
     x = np.fft.irfft(thermal_response)[int(numbins/2-1):-int(numbins/2-1)] # Reverse fourier transform to time domain and throw away start and end to get rid of finite time effects
     x = x*np.sqrt(np.trapz(np.abs(thermal_response**2), w/2/np.pi)/np.var(x)) # Scale to correct units
     x += ir # Add impulse response
@@ -77,10 +85,11 @@ def generate_random_numbers(seeds, Snn, numbins, maxw):
     numbins: number of frequency bins
     maxw: maximum frequency in frequency domain in rad/s
     """
-    np.random.seed(seeds[0])
+
+    np.random.seed(int(seeds[0]))
     randomlist = np.random.normal(0, 0.5*np.sqrt(2), numbins)
-    np.random.seed(seeds[1])
+    np.random.seed(int(seeds[1]))
     randomlist2 = np.random.normal(0, 0.5*np.sqrt(2), numbins)
-    np.random.seed(seeds[2])
+    np.random.seed(int(seeds[2]))
     randomlist3 = np.random.normal(0, np.sqrt(Snn*maxw), numbins)
     return randomlist, randomlist2, randomlist3
