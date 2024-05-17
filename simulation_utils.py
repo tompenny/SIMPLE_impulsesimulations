76ae8d191ba901d0230164647725ff392b5c079e
Thomas Penny <thomaspenny284@gmail.com>
2024-05-17 10:17:04 -0400

    Added frequency modulated displacement generation

--- simulation_utils.py
+++ simulation_utils.py
@@ -166,6 +166,38 @@ def impulse_resp_fm(time, t0, A, y0, yfb, w0, fm):
             output[n] =  A*np.sin(w1*(t-t0))*np.exp(-y1*(t-t0))
     return output
 
+def generate_displacement_fm(w, w0, y0, yfb, M, T, rnd, rnd2, rnd3, ir, iter, phase):
+    """
+    Generates the frequency domain response of a particle then reverse fourier transforms to the time domain
+    Frequency modulates the oscillator by modulating the timestep
+    Adds an impulse response (that must also be frequency modulated) and imprecision noise in the time domain
+    Returns displacement
+    w: frequency bins for frequency domain response
+    w0: natural frequency of particle
+    y0: intrinsic damping of particle (gas and/or laser)
+    yfb: Additional damping from cold feedback mechanism
+    M: mass of oscillator
+    T: environment temperature
+    rnd and rnd2: Noise for fourier domain - should each have 0 mean and 1/sqrt(2) width I think
+    rnd3: imprecision noise - should be 0 mean with width of Snn/2/dtn where Snn of single-sided PSD noise value in m^2/Hz and dtn is timestep in s (1/(2*(max frequency in Hz)))
+    ir: impulse response - must have same number of points as 2*len(w)
+    iter: modulation depth of frequency modulation
+    phase: phase of sawtooth for frequency modulation
+    """
+
+    numbins = len(w)
+    # Generate frequency response of particle
+    thermal_response = transfer_function2(w, w0, y0, yfb, rnd, rnd2, M, T)
+    x = np.fft.irfft(thermal_response)[int(numbins/2-1):-int(numbins/2-1)] # Reverse fourier transform to time domain and throw away start and end to get rid of finite time effects
+    x = x*np.sqrt(np.trapz(np.abs(thermal_response**2), w/2/np.pi)/np.var(x)) # Scale to correct units
+    time = np.linspace(0, np.pi/w[-1]*numbins, numbins)
+    time_mod = generate_sawtooth_frequency_modulation(time, iter, phase) # modulate time step intervals
+    x = frequency_modulation(x, time, time_mod) # interpolate back to fixed time base
+    x += ir # Add impulse response
+    x += rnd3 # Add measurement noise
+
+    return x
+
 def save_data_hd5f(filename, data):
     """
     Saves data in HDF5. Does it in a simple way by looping through data and datasetnames
